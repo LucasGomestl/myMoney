e93d568d609e675db662f5e3a31fa99260eec734
Lucas Gomes <lucasgomes_tl@hotmail.com>
2019-12-13 22:41:27 -0400

    frontend first commit

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+export default props => (
+    <h1>Test</h1>
+)
---
+++ frontend/webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack')
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/public'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public',
+    },
+    resolve: {
+        extensions: [' ', '.js', '.jsx'],
+        alias: {
+            modules: __dirname + '/node_modules',
+            src: __dirname + '/src',
+            jquery: 'modules/admin-lte/bower_components/jquery/dist/jquery.min.js',
+            bootstrap: 'modules/admin-lte/bower_components/bootstrap/js/bootstrap.min.js'
+        }    
+    },  
+    plugins: [
+        new MiniCssExtractPlugin({filename:'app.css'}),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            Jquery: 'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ], 
+    module: {
+        rules: [{
+            test: /.js[x]?$/,
+            loader: 'babel-loader',
+            exclude: '/node_modules',
+            options: {
+                presets: ['es2015', 'react'],
+                plugins: ['transform-object-rest-spread']
+            }
+        },{
+            test: /\.css$/,
+            use: 
+            [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+            ]
+        }, {
+            test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+            use: ['file-loader']
+        }]
+    }
+}
