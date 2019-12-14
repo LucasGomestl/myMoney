019d94260dfb68e8af78d0707d2acb6c8405f12e
Lucas Gomes <lucasgomes_tl@hotmail.com>
2019-12-14 16:55:10 -0400

    Create routes component

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {HashRouter} from 'react-router-dom'
 
 import '../common/template/dependencies'
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
@@ -6,15 +7,20 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
 
 
 export default props => (
-    
-    <React.Fragment>
-        <Header />
-        <SideBar />
-    </React.Fragment>
-    
-    
+    <HashRouter>
+        <div className="wrapper">
+            <Header />
+            <SideBar />
+
+            <Routes />
+
+            <Footer />
+        </div>
+    </HashRouter>
     
 )
