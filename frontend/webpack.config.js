fc37bbbbcff409dc7483cf8eda836bdb1503dc78
Lucas Gomes <lucasgomes_tl@hotmail.com>
2019-12-20 12:16:52 -0400

    Change server port

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         path: __dirname + '/public'
     },
     devServer: {
-        port: 8080,
+        port: 8084,
         contentBase: './public',
     },
     resolve: {
