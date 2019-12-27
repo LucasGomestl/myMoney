07a61a61f7a24372e8c3aed74f2fd8e9c7fbcb3b
Lucas Gomes <lucasgomes_tl@hotmail.com>
2019-12-26 23:18:54 -0400

    Update menu item label

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -6,7 +6,7 @@ import MenuTree from './menuTree'
 export default props => (
     <ul className="sidebar-menu tree" data-widget='tree'>
         <MenuItem path='#' label='Dashboard' icon='dashboard' />
-        <MenuTree label='Cadastro' icon='edit'>
+        <MenuTree label='Register' icon='edit'>
             <MenuItem path='#/billingCycles' label='Billing Cycles' icon='usd' />
         </MenuTree>
     </ul>
